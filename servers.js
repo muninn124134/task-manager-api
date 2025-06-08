82b13fb9a4601602da251b30e554b9e7f175e9cd
muninn124134 <pedrocks127@gmail.com>
2025-06-08 20:48:20 -0300

    feat: initial commit

---
+++ servers.js
@@ -0,0 +1,17 @@
+// server.js
+const mongoose = require('mongoose');
+const app = require('./app');
+const dotenv = require('dotenv');
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => {
+  console.log('Connected to MongoDB');
+  app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server running on port ${process.env.PORT || 3000}`);
+  });
+})
+.catch((err) => console.error('MongoDB connection error:', err));
