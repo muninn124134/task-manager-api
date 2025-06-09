b2d70d3ad1ce269dc93885535e3ca2b5a65fb18a
muninn124134 <pedrocks127@gmail.com>
2025-06-08 21:56:23 -0300

    feat: create login in authController

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -21,3 +21,29 @@ exports.register = async (req, res, next) => {
     
     } catch (err) {next(err)}
 }
+
+exports.login = async (req, res, next) => {
+    try {
+        const { email, password } = req.body
+        if (!email || !password) return res.status(400).json({ message: 'Email and password are required'})
+
+        const user = await User.findOne({ email })
+        if (!user) return res.status(401).json({ message: 'Invalid credentials'})
+
+        const correctPassword = await bcrypt.compare(password, user.password)
+        if (!correctPassword) return res.status(401).json({ message: 'Invalid credentials'})
+
+        const token = generateToken(user._id)
+
+        res.status(200).json({
+            token,
+            user: {
+                id: user._id,
+                name: user.name,
+                email: user.email,
+                balance: user.balance,
+            }
+        })
+    } catch (err) { next(err) }
+
+}
