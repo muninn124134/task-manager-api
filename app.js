9647043a0b162310c7942b7f0956bf37edcfbda7
muninn124134 <pedrocks127@gmail.com>
2025-06-08 21:05:05 -0300

    feat: create server.js app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(morgan('dev'))
+
+const authRoutes = require('./src/routes/authRoutes')
+app.use('/api/v1/auth', authRoutes)
+
+app.use((req, res, next) => {
+    res.status(404).json({ 'message': 'Route not found'})
+})
+
+app.use((err, req, res, next) => {
+    console.error(err.stack)
+    res.status(err.statusCode || 500).json( {
+        'error' : err.message || 'Internal error'
+    })
+})
